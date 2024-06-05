62e370cd78cdbdf5fbbed822b121a4052ef2803c
JoseAngelGuerreroInzunza <158419935+JoseAngelGuerreroInzunza@users.noreply.github.com>
2024-06-05 15:18:07 +0000

    actualizacion

--- metodoBur/MetodoBur.java
+++ metodoBur/MetodoBur.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 //Proposito: Elaborar un programa que obtenga 10 números aleatorios entre 1 al 100  y los almacene en un arreglo, 
 //luego los ordene utilizando en método de la burbuja y los imprima de mayor a menor.
 
-//Link del repositorio: https://github.com/AED1285-FUP-GA-EJ24/GHUB.java.EJ2024.UnidadV.JoseAngel
+//Link del repositorio: https://github.com/AED1285-FUP-GAEJ24/GHUB.java.EJ2024.UnidadV.JoseAngel
 
 public class MetodoBur{
     public static void metodoBur(){
@@ -18,8 +18,8 @@ public class MetodoBur{
         }
         System.out.println("");
         System.out.println("Números ingresados:");
-        for (int numero : numeros) {
-            System.out.print(numero + " ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
         }
         int n = numeros.length;
         for (int i = 0; i < n-1; i++) {
@@ -33,15 +33,15 @@ public class MetodoBur{
         }
         System.out.println("");
         System.out.println("Números ordenados de mayor a menor:");
-        for (int numero : numeros) {
+        for (int i = 0; i < numeros.length; i++) {
+            int numero = numeros[i];
             System.out.print(numero + " ");
         }
         System.out.println("");
         teclado.close();
         
     }
-
-        }
+}
     
 
 
